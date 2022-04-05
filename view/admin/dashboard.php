39b92291cde5fc0ef7b8cf010551a5a079c84dab
teemo24 <tounsi.online@gmail.com>
2022-04-05 14:39:35 +0100

    update

--- view/admin/dashboard.php
+++ view/admin/dashboard.php
@@ -1,115 +1,35 @@
 <?php include "header.php"; ?>
-<div class="container">
-    <header>
-        <hgroup class="logo span-6">
-        
-        </hgroup>
-        <nav class="session-nav span-6">
-            <ul>
-            <li class="show-menu">
-                <a href="">
-                <span class="fa fa-bars"></span>
-                </a>
-            </li>
-            <li class="account-settings">
-                <a href="">
-                <span class="fa fa-user"></span>
-                </a>
-            </li>
-            <li class="logout">
-                <a href="">
-                <span class="fa fa-close"></span>
-                </a>
-            </li>
-            </ul>
-        </nav>
-    </header>
+<div class="sp-lg"></div>
+<div class="sp-lg"></div>
+<div class="container-fluid">
+  <div class="controls">
+    <button class="add"><i class="bi bi-person-plus"></i></button>
+  </div>
+  <table class="table table-striped table-hover">
+    <thead>
+      <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nom et prénom</th>
+      <th scope="col">Téléphone</th>
+      <th scope="col">Date d'inscription</th>
+      <th scope="col">Etat</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">#</th>
+      <td>Mark</td>
+      <td>12345678</td>
+      <td>02/03/2022</td>
+      <td>Active</td>
+      <td class="actions">
+      <a href="#"><i class="bi bi-eye show-client"></i></a>
+      <a href="#"><i class="bi bi-trash delete-client"></i></a>
+      <a href="#"><i class="bi bi-pencil edit-client"></i></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
 </div>
-
-
-  <nav class="application-nav">
-    <div class="container">
-      <ul>
-        <li class=""><a href=""><span class="fa fa-home"></span><span class="text">Dashboard</span></a></li>
-        <li class=""><a href=""><span class="fa fa-file"></span><span class="text">Pages</span></a></li>
-        </ul>
-    </div>
-  </nav>
-
-
-  <section id="main-content">
-    <div class="container">
-      <div class="span-12">
-        <nav class="breadcrumb-nav">
-          <a href="#">Dashboard</a> &raquo;
-          <a href="#">Pages</a> &raquo;
-          About Us
-        </nav>
-      </div>
-
-      <div class="span-6">
-        <div class="panel">
-          <label class="panel-header" for="page-title">
-            Page Title
-            <span class="hint">The title of the page as displayed by a web browser.</span>
-          </label>
-          <div class="panel-body">
-            <input id="page-title" type="text" value="About Us" />
-          </div>
-        </div>
-      </div>
-
-      <div class="span-6">
-        <div class="panel">
-          <label class="panel-header" for="page-url">
-            URL
-            <span class="hint">http://www.example.com/about</span>
-          </label>
-          <div class="panel-body">
-            <input id="page-url" type="text" value="/about" />
-          </div>
-        </div>
-      </div>
-
-      <div class="span-12">
-        <div class="panel">
-          <label class="panel-header" for="page-main-content">
-            Main Content
-            <span class="hint">Say something about your company.</span>
-          </label>
-          <div class="panel-body">
-            <div class="wysiwyg">
-              <span class="fa fa-bold"></span>
-              <span class="fa fa-italic"></span>
-              <span class="fa fa-underline"></span>
-              <span class="fa fa-strikethrough"></span>
-              <span class="fa fa-list-ul"></span>
-              <span class="fa fa-list-ol"></span>
-              <span class="fa fa-link"></span>
-              <span class="fa fa-image"></span>
-            </div>
-            <textarea id="page-main-content"></textarea>
-          </div>
-        </div>
-      </div>
-
-      <div class="span-12">
-        <div class="panel">
-          <label class="panel-header" for="page-mission-statement">
-            Mission Statement
-            <span class="hint">Your company's mission statment.</span>
-          </label>
-          <div class="panel-body">
-            <div class="wysiwyg">
-              <span class="icon-bold"></span>
-              <span class="icon-italic"></span>
-              <span class="icon-underline"></span>
-              <span class="icon-strikethrough"></span>
-            </div>
-            <textarea id="page-mission-statement"></textarea>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
 <?php include "footer.php"; ?>
--- view/admin/header.php
+++ view/admin/header.php
@@ -4,10 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="view/assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="view/assets/css/style.css?<?=time()?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <title>Wedding</title>
 </head>
 <body>
 
-    
+<nav class="navbar navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Dashboard</a>
+    <button class="navbar-toggler" type="button">
+        <i class="bi bi-box-arrow-right"></i>
+    </button>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Dashboard</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Clients</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Espaces</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Events</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Settings</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>    
